acc32403a38d142ca4d6a6f7118c2ac7acb1ec05
MonolithLover <oplustilada@gmail.com>
2026-02-22 19:41:13 +0100

    Memory tracking, object loading, fixes

--- dev/src/zenithra_events.c
+++ dev/src/zenithra_events.c
@@ -5,29 +5,27 @@ bool zenithra_handle_event_poll(struct InEngineData *engine_data_str){
 	bool program_should_quit = false;
 	const Uint8* key_state = SDL_GetKeyboardState(NULL);
 
-	while(SDL_PollEvent(&event) != 0){
+	while(SDL_PollEvent(&event)){
 		if(event.type == SDL_QUIT){
 			program_should_quit = true;
 		}
 
-		if(key_state[SDL_SCANCODE_ESCAPE] && !engine_data_str->focus_lost && !engine_data_str->KEYS->escape){
-			SDL_ShowCursor(SDL_ENABLE);
-			SDL_SetRelativeMouseMode(SDL_FALSE);
-			SDL_SetWindowGrab(engine_data_str->SDL->window, SDL_FALSE);
-			engine_data_str->focus_lost = true;
-			engine_data_str->KEYS->escape = true;
-		}
-		if(key_state[SDL_SCANCODE_ESCAPE] && engine_data_str->focus_lost && !engine_data_str->KEYS->escape){
-			SDL_ShowCursor(SDL_DISABLE);
-			SDL_SetRelativeMouseMode(SDL_TRUE);
-			SDL_SetWindowGrab(engine_data_str->SDL->window, SDL_TRUE);
-			engine_data_str->focus_lost = false;
-			engine_data_str->KEYS->escape = true;
-		}
+		if(event.type == SDL_KEYDOWN && !event.key.repeat){
+			if(event.key.keysym.scancode == SDL_SCANCODE_ESCAPE && !engine_data_str->focus_lost){
+				SDL_ShowCursor(SDL_ENABLE);
+				SDL_SetRelativeMouseMode(SDL_FALSE);
+				SDL_SetWindowGrab(engine_data_str->SDL->window, SDL_FALSE);
+				engine_data_str->focus_lost = true;
 
-		if(engine_data_str->KEYS->escape){
-			if(!key_state[SDL_SCANCODE_ESCAPE]){
-				engine_data_str->KEYS->escape = false;
+				event.key.keysym.scancode = 0;
+			}
+			if(event.key.keysym.scancode == SDL_SCANCODE_ESCAPE && engine_data_str->focus_lost){
+				SDL_ShowCursor(SDL_DISABLE);
+				SDL_SetRelativeMouseMode(SDL_TRUE);
+				SDL_SetWindowGrab(engine_data_str->SDL->window, SDL_TRUE);
+				engine_data_str->focus_lost = false;
+
+				event.key.keysym.scancode = 0;
 			}
 		}
 	}
--- dev/src/zenithra_main.c
+++ dev/src/zenithra_main.c
@@ -4,6 +4,9 @@
 void zenithra_test_editor(struct InEngineData *engine_data_str);
 
 int main(int argc, char *argv[]){
+    DEV_MODE = true;
+    GAME_DIMENSIONALITY = 2;
+
     struct InEngineData *engine_data_str = zenithra_init(1200, 700);
 
     zenithra_test_editor(engine_data_str);
@@ -21,26 +24,12 @@ void zenithra_test_editor(struct InEngineData *engine_data_str){
         current_frame_time = SDL_GetPerformanceCounter();
         engine_data_str->delta_time = (double)((current_frame_time - last_frame_time) * 1000 / (double)SDL_GetPerformanceFrequency());
 
-        /*Uint32 mouse_button_pressed = SDL_GetMouseState(NULL, NULL);
-        if(!engine_data_str->focus_lost){
-            zenithra_calc_mouse_movement(engine_data_str);
-
-            if(SDL_BUTTON(3) == mouse_button_pressed){
-                zenithra_update_position(engine_data_str);
-            }
-        }*/
-
-        RENDERER_BUF *next_in_line = engine_data_str->RENDERER_BUF;
-        while(next_in_line){
-            SDL_SetRenderDrawColor(engine_data_str->SDL->renderer, next_in_line->r, next_in_line->g, next_in_line->b, next_in_line->w);
-            SDL_RenderDrawPoint(engine_data_str->SDL->renderer, next_in_line->x, next_in_line->y);
-
-            next_in_line = next_in_line->next;
-        }
-
+        zenithra_draw(engine_data_str);
 
         program_should_quit = zenithra_handle_event_poll(engine_data_str);
 
         SDL_RenderPresent(engine_data_str->SDL->renderer);
+
+        zenithra_check_and_display_memory_change(engine_data_str);
     }while(!program_should_quit);
 }
